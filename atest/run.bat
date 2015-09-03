0e0dce8696aeb32bd920d0735cbcf48dd59fb421
Pekka Klärck <peke@iki.fi>
2015-09-03 13:19:28 +0300

    Updated atest/run.bat

--- atest/run.bat
+++ atest/run.bat
@@ -5,7 +5,5 @@ set BINPATH=%ROOTDIR%\bin
 set SRCPATH=%ROOTDIR%\src
 set TESTDATA=%ROOTDIR%\atest\vacalc
 set TESTLIBS=%ROOTDIR%\atest\libraries
-set CLASSPATH=%BINPATH%\robotframework-2.6.3.jar;%TESTLIBS%\swinglibrary-1.2.jar;%BINPATH%
+set CLASSPATH=%BINPATH%\robotframework-2.9.1.jar;%TESTLIBS%\swinglibrary-1.9.4.jar;%BINPATH%
 java org.robotframework.RobotFramework -P "%SRCPATH%" -P "%TESTLIBS%" %* --outputdir "%ROOTDIR%\results" --critical regression "%TESTDATA%"
-
-
